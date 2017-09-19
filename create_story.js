7542cc0c9d8addaaef1b2fe877e1de68a6bd1ba7
RifatTauwab <mdruts@gmail.com>
2017-09-19 18:12:49 +0600

    handle html file to show all news story titles

--- create_story.js
+++ create_story.js
@@ -34,6 +34,34 @@ app.post('/create_story', urlencodedParser, function (req, res) {
    res.end(JSON.stringify(response));
 })
 
+app.get('/story', function (req, res) {
+   res.sendFile( __dirname + "/" + "story_list.html" );
+})
+app.get('/get_story', function (req, res) {
+	
+	var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/news";
+    var stories = "";
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        //Find all documents in the customers collection:
+        db.collection("story").find({},
+		{
+		 _id: false, 
+		 story_title: true, 
+		 story_description: true,
+		 story_author: true,
+		 story_date: true
+		}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+		res.send(result);
+        db.close();
+        });
+    });
+   //res.sendFile( __dirname + "/" + "story_list.html" );
+})
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
